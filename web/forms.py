288ab17bf4cfe93e184311b3ad38f93365252989
TeriForey <teri.forey@gmail.com>
2019-10-15 14:38:57 +0100

    Added honeypot to contact form, and renamed contact form fields

--- web/forms.py
+++ web/forms.py
@@ -9,14 +9,18 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = models.Contact
         widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'Your Name'}),
-            'email': forms.TextInput(attrs={'placeholder': 'Your email address'}),
+            'nameval': forms.TextInput(attrs={'placeholder': 'Your Name'}),
+            'emailval': forms.TextInput(attrs={'placeholder': 'Your email address'}),
             'variant': forms.TextInput(
                 attrs={'placeholder': 'Variant description required for variant analysis errors'}),
             'question': forms.Textarea(
                 attrs={'placeholder': 'Enter query here'}),
         }
-        fields = ('name', 'email', 'variant', 'question')
+        fields = ('nameval', 'emailval', 'variant', 'question')
+
+    def clean(self):
+        if self.data['name'] or self.data['email']:
+            raise forms.ValidationError("Spam detected")
 
 
 class BatchValidateForm(forms.Form):
--- web/models.py
+++ web/models.py
@@ -3,15 +3,15 @@ from django.utils import timezone
 
 
 class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+    nameval = models.CharField(max_length=100, verbose_name='Name')
+    emailval = models.EmailField(verbose_name='Email')
     variant = models.CharField(max_length=100, null=True, blank=True)
     question = models.TextField()
     asked = models.DateTimeField(default=timezone.now)
     answered = models.BooleanField(default=False)
 
     def __str__(self):
-        return "%s - %s (dealt with: %s)" % (self.name, self.asked.date(), self.answered)
+        return "%s - %s (dealt with: %s)" % (self.nameval, self.asked.date(), self.answered)
 
     def send_email(self):
         """
