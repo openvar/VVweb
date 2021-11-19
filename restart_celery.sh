d88e2c4cb2a251369f70ed0e9029db5b8c591956
Peter Causey-Freeman <pjf9@leicester.ac.uk>
2021-11-19 10:49:41 +0000

    Add celery re-start shell script for Chron Job

---
+++ restart_celery.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Start environment
+conda activate vvweb
+
+# Stop celery
+ps aux | grep celery | awk '{print $2}' | xargs kill
+
+# Purge the batch queue
+celery purge --force
+
+# Start celery
+celery beat -A VVweb -l error --scheduler django_celery_beat.schedulers.DatabaseScheduler --detach --logfile logs/celery/beat.log --pidfile celerybeat.pid
+celery worker -A VVweb -l error --detach --logfile logs/celery/%n%I.log --pidfile celeryd.pid
+
+
+# <LICENSE>
+# Copyright (C) 2016-2021 VariantValidator Contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# </LICENSE>
