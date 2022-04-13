91e4872fa58f5853126a3bc229aa136152d010af
Peter Causey-Freeman <pjf9@leicester.ac.uk>
2022-04-13 16:43:16 +0100

    add inspect celery

--- run_celery.sh
+++ run_celery.sh
@@ -4,10 +4,7 @@
 # These are the hashed lines and celery was set to 4.4.6
 
 celery beat -A VVweb -l error --scheduler django_celery_beat.schedulers.DatabaseScheduler --detach --logfile logs/celery/beat.log --pidfile celerybeat.pid
-# celery beat -A VVweb -l error --scheduler django_celery_beat.schedulers.DatabaseScheduler --logfile logs/celery/beat.log --pidfile celerybeat.pid &
-
 celery worker -A VVweb -l error --detach --logfile logs/celery/%n%I.log --pidfile celeryd.pid
-# celery worker -A VVweb -l error --logfile logs/celery/%n%I.log --pidfile celeryd.pid &
 
 # <LICENSE>
 # Copyright (C) 2016-2022 VariantValidator Contributors
