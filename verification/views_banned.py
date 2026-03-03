1035edb1eba0dd44162fd05b6d851552bf9f1246
Peter-J-Freeman <peter.j.freeman@manchester.ac.uk>
2026-03-03 15:01:03 +0000

    Update banned logic

--- verification/views_banned.py
+++ verification/views_banned.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
-@login_required
 def banned_landing(request):
     return render(request, "banned.html", {"profile": request.user.profile})
