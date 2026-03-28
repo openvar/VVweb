dc923ebbd37ea7a341d20e4d8960e46a0694f05b
Peter-J-Freeman <peter.j.freeman@manchester.ac.uk>
2026-03-28 00:22:52 +0000

    security upgrades and legacy vcf2hgvs removal

--- at_reboot.sh
+++ at_reboot.sh
@@ -1,7 +1,7 @@
 sleep 30
 source /local/miniconda3/bin/activate vvweb_v2
 cd /local/VVweb
-./kill_celery
+./kill_celery.sh
 ./run_celery.sh
 echo "test" |mail -s "$HOSTNAME VVWeb INFO Celery has been started (at_reboot.sh)" peter.j.freeman@manchester.ac.uk
 
