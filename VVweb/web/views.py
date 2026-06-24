0fed2ccd4bcb6c6336cda747b783939bb575e6ed
Peter-J-Freeman <peter.j.freeman@manchester.ac.uk>
2026-06-24 20:55:00 +0100

    Various updates added by bug identification

--- VVweb/web/views.py
+++ VVweb/web/views.py
@@ -207,7 +207,7 @@ def genes_to_transcripts(request):
                     if ref.startswith("LRG"):
                         xml_id = ref.split("t")[0]
                         trans["url"] = (
-                            f"http://ftp.ebi.ac.uk/pub/databases/lrgex/{xml_id}.xml"
+                            f"https://ftp.ebi.ac.uk/pub/databases/lrgex/{xml_id}.xml"
                         )
                     elif ref.startswith('ENST'):
                         trans['url'] = f"https://www.ensembl.org/Homo_sapiens/Transcript/Summary?db=core;t={ref}"
